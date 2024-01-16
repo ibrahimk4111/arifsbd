a45183ae63b37afaa0700f3870d1e621a919ca3d
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-01-17 03:52:30 +0600

    client fixs

--- src/components/Partners/Clients.jsx
+++ src/components/Partners/Clients.jsx
@@ -8,18 +8,18 @@ import "swiper/css"
 import { Link } from 'react-router-dom';
 
 const Clients = () => {
-    const { partnerData } = useContext(UserContext);
-    const ourClients = partnerData;
+  const { partnerData } = useContext(UserContext);
+  const ourClients = partnerData;
 
-    return (
-        <div id='partners' className='pb-10 pt-5'>
-            <Container className=' bg-slate-100 py-5 rounded-lg'>
-                <div className='relative w-full'>
-                    <h1 className='uppercase font-semibold text-xl text-slate-400'> Our Clients </h1>
-                </div>
-                <hr className=' h-[2px] bg-theme my-2 rounded-full w-full mb-5' />
+  return (
+    <div id='partners' className='pb-10 pt-5'>
+      <Container className=' bg-slate-100 py-5 rounded-lg'>
+        <div className='relative w-full'>
+          <h1 className='uppercase font-semibold text-xl text-slate-400'> Our Clients </h1>
+        </div>
+        <hr className=' h-[2px] bg-theme my-2 rounded-full w-full mb-5' />
 
-                {/* <Swiper
+        {/* <Swiper
           speed={1300}
           freeMode={true}
           autoplay={{
@@ -48,27 +48,25 @@ const Clients = () => {
           modules={[Autoplay, FreeMode]}
           className="pt-5 px-2 pb-2"
         > */}
-                <section className=' grid justify-center items-center lg:grid-cols-8 grid-cols-4 gap-3 p-2'>
-                    {
-                        ourClients && ourClients.map((item, index) => (
-                            // <SwiperSlide key={index} className=' h-auto '>
-                            <Link key={index} to={item.website} target='_blank'>
-                                <div title={item.name} className='w-full  bg-white rounded-md group'>
-                                    <div className=' shadow shadow-slate-400 rounded-md lg:h-full h-auto'>
-                                        <div className=' col-span-4 flex justify-center items-center h-14 overflow-hidden '>
-                                            <img src={`${item.img}`} alt={index + 1} className='object-cover w-auto h-auto max-h-full p-1' loading='lazy' />
-                                        </div>
-                                    </div>
-                                </div>
-                            </Link>
-                            // </SwiperSlide>
-                        ))
-                    }
-                </section>
-                {/* </Swiper> */}
-            </Container>
-        </div>
-    )
+        <section className=' flex flex-wrap justify-center items-center gap-3 p-2'>
+          {
+            ourClients && ourClients.map((item, index) => (
+              // <SwiperSlide key={index} className=' h-auto '>
+              <Link key={index} to={item.website} target='_blank'>
+                <div title={item.name} className='w-full  bg-white rounded-md group h-auto'>
+                  <div className=' shadow shadow-slate-400 rounded-md flex justify-center items-center h-14 w-28 overflow-hidden '>
+                    <img src={`${item.img}`} alt={index + 1} className='object-cover w-auto h-auto max-h-full p-1' loading='lazy' />
+                  </div>
+                </div>
+              </Link>
+              // </SwiperSlide>
+            ))
+          }
+        </section>
+        {/* </Swiper> */}
+      </Container>
+    </div>
+  )
 }
 
 export default Clients

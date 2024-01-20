345c250cbac364b6cabaddc730e4aedb932ccd6d
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-01-21 02:39:23 +0600

    why choose us

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -38,7 +38,7 @@ const WhyChooseUs = () => {
                         <Accordion className=' shadow hover:shadow-lg shadow-slate-300 '>
                             <AccordionSummary onClick={() => { setIsVisible(prev => !prev) }} className={`${isVisible ? "bg-theme rounded-t-md text-white" : ""} transition-all duration-300 ease-in h-20`} expandIcon={!isVisible ? <KeyboardArrowDown className=' font-semibold' /> : <CloseRounded />}>
                                 <Box>
-                                    <Typography className=' text-xl px-2 font-semibold'> 1. What is the price for your medicines and equipments offerings ? </Typography>
+                                    <Typography className=' text-lg px-2 font-semibold'> 1. What is the price for your medicines and equipments offerings ? </Typography>
                                 </Box>
                             </AccordionSummary>
                             <AccordionDetails className={`${isVisible ? "scale-y-100 opacity-100 " : "scale-y-0 opacity-0"} transition-all duration-300 ease-in border-l-8 border-black p-5 leading-7 text-base`}>
@@ -49,33 +49,33 @@ const WhyChooseUs = () => {
                         <Accordion className=' shadow hover:shadow-lg shadow-slate-300 '>
                             <AccordionSummary onClick={() => { setIsVisible2(prev => !prev) }} className={`${isVisible2 ? "bg-theme rounded-t-md text-white" : ""} transition-all duration-300 ease-in h-20`} expandIcon={!isVisible2 ? <KeyboardArrowDown className=' font-semibold' /> : <CloseRounded />}>
                                 <Box>
-                                    <Typography className=' text-xl px-2 font-semibold'> 2. What is the available payment Options ? </Typography>
+                                    <Typography className=' text-lg px-2 font-semibold'> 2. What is the available payment Options ? </Typography>
                                 </Box>
                             </AccordionSummary>
                             <AccordionDetails className={`${isVisible2 ? "scale-y-100 opacity-100 " : "scale-y-0 opacity-0"} transition-all duration-300 ease-in border-l-8 border-black p-5 leading-7 text-base`}>
-                                <p>Our product prices fluctuate based on particle size and purity, including forms like powder, granules, aggregates, or lumps.</p>
+                                <p>TT Advance, Letter of Credit at Sight & Confirmed Letter of Credit.</p>
                             </AccordionDetails>
                         </Accordion>
 
                         <Accordion className=' shadow hover:shadow-lg shadow-slate-300 '>
                             <AccordionSummary onClick={() => { setIsVisible3(prev => !prev) }} className={`${isVisible3 ? "bg-theme rounded-t-md text-white" : ""} transition-all duration-300 ease-in h-20`} expandIcon={!isVisible3 ? <KeyboardArrowDown className=' font-semibold' /> : <CloseRounded />}>
                                 <Box>
-                                    <Typography className=' text-xl px-2 font-semibold'> 3. What is the available delivery Options ? </Typography>
+                                    <Typography className=' text-lg px-2 font-semibold'> 3. What is the available delivery Options ? </Typography>
                                 </Box>
                             </AccordionSummary>
                             <AccordionDetails className={`${isVisible3 ? "scale-y-100 opacity-100 " : "scale-y-0 opacity-0"} transition-all duration-300 ease-in border-l-8 border-black p-5 leading-7 text-base`}>
-                                <p>Our product prices fluctuate based on particle size and purity, including forms like powder, granules, aggregates, or lumps.</p>
+                                <p>FOB, CFR & CIF, FOB - Free on Board ( Exporter Port ), CFR - Cost and Freight to importer destination Port, CIF - Cost, Insurance & Freight to importer destination Port.</p>
                             </AccordionDetails>
                         </Accordion>
 
                         <Accordion className=' shadow hover:shadow-lg shadow-slate-300 '>
                             <AccordionSummary onClick={() => { setIsVisible4(prev => !prev) }} className={`${isVisible4 ? "bg-theme rounded-t-md text-white" : ""} transition-all duration-300 ease-in h-20`} expandIcon={!isVisible4 ? <KeyboardArrowDown className=' font-semibold' /> : <CloseRounded />}>
                                 <Box>
-                                    <Typography className=' text-xl px-2 font-semibold'> 4. How can i track my order status ? </Typography>
+                                    <Typography className=' text-lg px-2 font-semibold'> 4. How can I track my order status ? </Typography>
                                 </Box>
                             </AccordionSummary>
                             <AccordionDetails className={`${isVisible4 ? "scale-y-100 opacity-100 " : "scale-y-0 opacity-0"} transition-all duration-300 ease-in border-l-8 border-black p-5 leading-7 text-base`}>
-                                <p>Our product prices fluctuate based on particle size and purity, including forms like powder, granules, aggregates, or lumps.</p>
+                                <p>Send email to <span className=' text-theme '>arifs@dhaka.net</span> or send <span className=' text-theme'>Tel : +88-02-41053435, +88-02-41053436, +88-02-41053437, +88-02-41053438 & FAX : +88-02-41053288</span></p>
                             </AccordionDetails>
                         </Accordion>
 
@@ -84,7 +84,7 @@ const WhyChooseUs = () => {
                 </div>
                 <div className=' lg:col-span-5' >
                     <div>
-                        <p className=' rounded-md p-3 lg:text-3xl text-4xl lg:[word-spacing:3px] lg:leading-relaxed leading-loosed mt-5 bg-slate-100'>Get a free quote here</p>
+                        <p className=' rounded-md p-1 lg:text-3xl text-4xl lg:[word-spacing:3px] lg:leading-relaxed leading-loosed bg-slate-100'>Get a free quote here</p>
                         <p className=' text-base [word-spacing:3px] leading-7 py-5 px-3 text-justify'>We understand the importance of every inquiry, we cater each request with utmost interest and concern. </p>
                     </div>
                     <div className=' grid grid-cols-1 gap-8'>
